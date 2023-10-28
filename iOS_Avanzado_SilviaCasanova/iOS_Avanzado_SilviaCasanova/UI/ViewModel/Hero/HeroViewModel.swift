00b0c3217798de6f8c9340a720cd9f1963ce71c5
silviaITI <silvia.casanovamartinez@gmail.com>
2023-10-28 17:34:33 +0200

    HeroesViewModel completed

--- iOS_Avanzado_SilviaCasanova/iOS_Avanzado_SilviaCasanova/UI/ViewModel/Hero/HeroViewModel.swift
+++ iOS_Avanzado_SilviaCasanova/iOS_Avanzado_SilviaCasanova/UI/ViewModel/Hero/HeroViewModel.swift
@@ -7,24 +7,37 @@
 
 import Foundation
 
-class HeroesViewModel {
+class HeroesViewModel: HeroesTableViewControllerDelegate {
+    var viewState: ((HeroesViewState) -> Void)?
+    
     var heroes: [Hero] = []
-//    
-//    func fetchHeroes() {
-//        ApiProvider.shared.getHeroes { result in
-//            switch result {
-//            case let .success(heroes):
-//                self.heroes = heroes
-//                DispatchQueue.main.async {
-//                    self.listadoHeroes.reloadData()
-//                }
-//                
-//            case let .failure(error):
-//                print("Error: \(error)")
-//            }
-//            self.hideLoading()
-//        }
-//        
-//    }
+    var heroesCount: Int {
+        return heroes.count
     }
+    
+    func  fetchHeroesList() {
+      
+        ApiProvider.shared.getHeroes { result in
+            switch result {
+            case let .success(heroes):
+                self.heroes = heroes
+                DispatchQueue.main.async {
+                    self.viewState?(.reloadData)
+                }
+                
+            case let .failure(error):
+                print("Error: \(error)")
+            }
+           
+        }
+        
+    }
+    func heroBy(index: Int) -> Hero? {
+        if index >= 0 && index < heroesCount {
+           return heroes[index]
+        } else {
+           return nil
+        }
+    }
+}
 
